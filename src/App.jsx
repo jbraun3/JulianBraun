f1ec556f62b740d9cf5e48953ccc6311dc3f8da5
Julian Anthony Braun <jbraun3@uw.edu>
2025-11-12 15:56:14 -0800

    dist folder yay

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>testing if update works</h1>
+      <h1>testing if update works :)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
